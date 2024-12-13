246d4883e2630276ab35c40143b860a2f55d67f7
User Name <nowramohammad@Nowras-MacBook-Pro.local>
2024-12-12 19:09:37 -0600

    adding files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Landing from "./components/Landing/Landing";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Signup from "./components/Signup/Signup";
+import SignInForm from "./components/SignInForm/SignInForm";
+import { getUser, signout} from "./services/authService";
+import Profile from "./components/Profile/Profile";
 
 function App() {
-  const [user, setUser] = useState(null)
+  // we will use this state to store the logged user data
+  const [user, setUser] = useState(getUser());
+  const navigate = useNavigate();
+  const handleSignout = () => {
+    signout();
+    setUser(null);
+  }
+  // Did not work
+  // if(!user){
+  //   navigate('/')
+  // }
 
   return (
     <>
-  <Navbar user={user} />
-<h1>Hello word!</h1>
+      <Navbar user={user}  handleSignout={handleSignout}/>
+      <Routes>
+        {user ? (
+          <Route path="/" element={<Dashboard user={user}/>} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
+
+        <Route path="/signup" element={<Signup setUser={setUser} />} />
+        <Route path='/signin' element={<SignInForm setUser={setUser} />} />
+        <Route path="/profile/:id" element={<Profile />}  />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/services/authService.js
@@ -0,0 +1,68 @@
+// src/services/authService.js
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+const signup = async (formData) => {
+  try {
+    const res = await fetch(`${BACKEND_URL}/users/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const json = await res.json();
+
+    console.log(json);
+    if (json.error) {
+      throw new Error(json.error);
+    }
+
+    // check for token and store it in local storage before returning the json
+    if(json.token){
+      localStorage.setItem("token", json.token);
+    }
+    return json;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const signin = async (formData) => {
+  try {
+    const res = await fetch(`${BACKEND_URL}/users/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const json = await res.json();
+    console.log(json);
+
+    if (json.error) {
+      throw new Error(json.error);
+    }
+    if (json.token) {
+      localStorage.setItem("token", json.token);
+      const user = JSON.parse(atob(json.token.split(".")[1]));
+      return user;
+    }
+  } catch (err) {
+    return { error: err.message };
+  }
+};
+
+
+const getUser = () => {
+  const token = localStorage.getItem("token");
+  if(!token) return null;
+  const user = JSON.parse(atob(token.split(".")[1]));
+  return user;
+}
+
+const signout = () => localStorage.removeItem("token");
+
+export { signup, signin, getUser, signout};
